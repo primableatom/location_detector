2230fe51153c156def16d3bf90beeae8700bc60a
Anup Biswal <anup.craver@gmail.com>
2019-08-31 23:39:43 +0530

    updated redis URL for heroku(production)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 
 Sidekiq.configure_client do |config|
   if Rails.env.production?
-    config.redis = { :size => 1, url: ENV["REDISTOGO_URL"] }
+    config.redis = { :size => 1, url: ENV["REDIS_URL"] }
   elsif Rails.env.development?
     config.redis = { :size => 1, url: "redis://127.0.0.1:6379/0" }
   end
@@ -10,7 +10,7 @@ end
 
 Sidekiq.configure_server do |config|
   if Rails.env.production?
-    config.redis = { url: ENV["REDISTOGO_URL"] }
+    config.redis = { url: ENV["REDIS_URL"] }
   elsif Rails.env.development?
     config.redis = { url: "redis://127.0.0.1:6379/0"} 
   end
